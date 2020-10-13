a92b70af1e0566b73e41f4bb658f0069810af20f
andreuva <andres.vicente.arevalo@gmail.com>
2020-10-13 11:14:44 +0100

    added gitignore and plank functions

---
+++ python/constants.py
@@ -0,0 +1,4 @@
+# Define some of the constants of the problem:
+c = 299792458                   # m/s
+h = 6.626070150e-34             # J/s
+kb = 1.380649e-23                # J/K
--- python/main.py
+++ python/main.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.special import wofz
 from tqdm import tqdm
+import constants as cte
 
 
 def Gaussian(x, alpha):
@@ -29,8 +30,20 @@ def Voigt(x, alpha, gamma):
         / sigma / np.sqrt(2*np.pi)
 
 
-# Define the constants of the problem:
-c = 299792458                   # m/s
+def plank_nu(nu, T):
+    """
+    Return the Plank function at a given temperature and frequency (IS)
+    """
+    return (2*cte.h*nu**3)/(cte.c**2) * (1/(np.exp(cte.h*nu/(cte.kb*T)) - 1))
+
+
+def plank_lamb(lamb, T):
+    """
+    Return the Plank function at a given temperature and wavelength (IS)
+    """
+    return (2*cte.h*cte.c**2)/(lamb**5) * \
+        (1/(np.exp(cte.h*cte.c/(lamb*cte.kb*T)) - 1))
+
 
 # We define the z0, zl, dz as our heigt grid (just 1D because of a
 # plane-parallel atmosfere and axial-simetry)
@@ -40,7 +53,22 @@ dz = 1e-1                           # define the stepsize
 zz = np.arange(zl, zu, dz)          # compute the 1D grid
 
 # Define the grid in frequencies ( or wavelengths )
-wl = c/(600*1e-9)           # Define the lower and upper frequency limit
-wu = c/(500*1e-9)
-dw = (wu-wl)/100            # Define 100 points to sample the spectrum
+wl = cte.c/(600*1e-9)           # Define the lower and upper frequency limit
+wu = cte.c/(500*1e-9)
+dw = (wu-wl)/100                # Define 100 points to sample the spectrum
 ww = np.arange(wl, wu, dw)
+
+'''# Check for the plank function to work properly
+xw = np.linspace(1e3, 1e15, 1e4)
+# xl = np.linspace(100e-9, 1500e-9, 1e4)
+yw = plank_nu(xw, 5778)
+# yl = plank_lamb(xl, 5778)
+plt.plot(xw, yw)
+plt.xlabel('frequency (Hz)')
+plt.ylabel('Spectral irradiance $W s sr^{-1} m^{-2}$')
+plt.title('Blackbody spectrum 5778K')
+plt.show()
+# plt.plot(xl, yl)
+# plt.show() '''
+
+# ------------------------ INITIAL CONDITIONS -----------------------------
