cba6f4c4be3ab4659f35cfa233e51d4dc5131229
andreuva <andres.vicente.arevalo@gmail.com>
2020-11-19 15:42:27 +0000

    added params.h

--- C/main.c
+++ C/main.c
@@ -8,13 +8,7 @@
 #include <math.h>
 #include <complex.h>    /* Standard Library of Complex Numbers */
 #include "gauleg.c"
-
-/* ------- DEFINE SOME OF THE CONSTANTS OF THE PROBLEM ---------- */
-const int c = 299792458;                   /* # m/s */
-const float h = 6.626070150e-34;             /* # J/s */
-const float kb = 1.380649e-23;                /* # J/K */
-const float R = 8.31446261815324;            /* # J/K/mol */
-const int T = 5778;                    /* # T (isotermic) of the medium */
+#include "params.h"
 
 /* ------ DEFINE THE PROBLEM PARAMETERS ------- */
 const float zl = -15; /*-log(1e3);          /* optical thicknes of the lower boundary */
---
+++ C/params.h
@@ -0,0 +1,6 @@
+/* ------- DEFINE SOME OF THE CONSTANTS OF THE PROBLEM ---------- */
+const int c = 299792458;                   /* # m/s */
+const float h = 6.626070150e-34;             /* # J/s */
+const float kb = 1.380649e-23;                /* # J/K */
+const float R = 8.31446261815324;            /* # J/K/mol */
+const int T = 5778;                    /* # T (isotermic) of the medium */
