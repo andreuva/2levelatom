11298e020a4088968d437ba8edd469f9e42f9243
andreuva <andres.vicente.arevalo@gmail.com>
2021-03-25 12:02:33 +0000

    added plots in py and debug in makefile

--- C/GPU/inversion/plots_inversion.py
+++ C/GPU/inversion/plots_inversion.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-_, I_sol, Q_sol, I_inv, Q_inv = np.loadtxt("../figures/profiles.txt",  delimiter=',', unpack=True)
+_, I_sol, Q_sol, I_inv, Q_inv, I_initial, Q_initial = np.loadtxt("figures/profiles_jkq.txt",  delimiter=',', unpack=True)
 
 plt.plot(I_sol, 'ok', label=r'$I/B_{\nu}$ "observed"')
 # plt.plot(I_initial, 'r', label=r'$I/B_{\nu}$ initial parameters')
@@ -9,6 +9,17 @@ plt.plot(I_inv, 'b', label=r'$I/B_{\nu}$ inverted parameters')
 plt.legend(); plt.xlabel(r'$\nu $')
 plt.show()
 # plt.plot(Q_initial, 'r--', label='$Q/I$ initial parameters')
+plt.plot(Q_sol, 'ok', label='$Q/I$ "observed"')
+plt.plot(Q_inv, 'b--', label='$Q/I$ inverted parameters')
+plt.legend(); plt.xlabel(r'$\nu $')
+plt.show()
+
+_, I_sol, Q_sol, I_inv, Q_inv = np.loadtxt("figures/profiles.txt",  delimiter=',', unpack=True)
+
+plt.plot(I_sol, 'ok', label=r'$I/B_{\nu}$ "observed"')
+plt.plot(I_inv, 'b', label=r'$I/B_{\nu}$ inverted parameters')
+plt.legend(); plt.xlabel(r'$\nu $')
+plt.show()
 plt.plot(Q_sol, 'ok', label='$Q/I$ "observed"')
 plt.plot(Q_inv, 'b--', label='$Q/I$ inverted parameters')
 plt.legend(); plt.xlabel(r'$\nu $')
